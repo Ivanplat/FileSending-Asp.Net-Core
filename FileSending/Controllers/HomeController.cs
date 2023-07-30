47e59d03d2a5b62e8e517a2b7896b785c1080f3d
Ivan Platonov <Platonov20030603@gmail.com>
2023-07-30 12:00:23 +0300

    Add project files.

---
+++ FileSending/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileSending.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _appEnvironment;
+        public HomeController(IWebHostEnvironment appEnvironment)
+        {
+            _appEnvironment = appEnvironment;
+        }
+        public IActionResult Index()
+        {
+            string filePath = Path.Combine(_appEnvironment.ContentRootPath,
+                @"Files\HelloFile.txt");
+            FileStream fs = new FileStream(filePath, FileMode.Open);
+            string fileType = "text/plain";
+            string fileName = "File.txt";
+            return File(fs, fileType, fileName);
+        }
+    }
+}
